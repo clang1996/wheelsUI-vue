b29e414189d018a07b2bd2f771e4eaed3d3d36a2
你çclang1996 <>
2020-07-03 21:31:41 +0800

    toast-demos.vue 第二次提交

--- docs/.vuepress/components/toast-demos.vue
+++ docs/.vuepress/components/toast-demos.vue
@@ -1,10 +1,26 @@
 <template>
 	<div>
 		<h4>预览</h4>
-		<div class="toast">
+		<div>
 			<g-button @click="toast1">上方弹出</g-button>
 			<g-button @click="toast2">下方弹出</g-button>
 			<g-button @click="toast3">中间弹出</g-button>
+			<h4>代码</h4>
+			<pre><code>{{content}}</code></pre>
+		</div>
+		<div>
+			<h2>自定义关闭按钮</h2>
+			<h4>预览</h4>
+			<div>
+				<g-button @click="toast4">上方弹出</g-button>
+			</div>
+			<h4>代码</h4>
+			<pre><code>{{content2}}</code></pre>
+		</div>
+		<div>
+			<h2>支持HTML</h2>
+			<h4>预览</h4>
+			<div>123</div>
 			<h4>代码</h4>
 			<pre><code>{{content}}</code></pre>
 		</div>
@@ -36,7 +52,16 @@
 				this.$toast('我是提示', {
 					position:'middle',
 				})
-			}
+			},
+			toast4() {
+				this.$toast('我是提示', {
+					position:'top',
+					closeButton:{
+						text:'我是关闭',
+						callback:() => {console.log('我是回调');}
+					}
+				})
+			},
 		},
 		data() {
 			return {
@@ -44,13 +69,20 @@
 <g-button @click="toast1">上方弹出</g-button>
 <g-button @click="toast2">下方弹出</g-button>
 <g-button @click="toast3">中间弹出</g-button>
-				`.trim()
+				`.trim(),
+				content2:`
+<g-button @click="toast4">上方弹出</g-button>
+methods:{
+  toast4() {
+    this.$toast('我是提示', {
+      position:'top',
+      closeButton:{
+        text:'我是关闭',
+        callback:()=>{console.log('我是回调')}
+    }
+  })
+}`.trim(),
 			}
 		}
 	}
 </script>
-<style lang="scss" scoped>
-	.toast{
-		z-index:30;
-		}
-</style>
